0839117815c760dd862879c4e3667708bed9dccc
anupriyashahdeo <anupriyashahdeo2001gmail.com>
2025-11-24 00:44:58 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup 
+setup(
+  name="medical_chatbot",
+  version="0.1.0",
+  author="Anupriya",
+  author_email="anupriyashahdeo2001@gmail.com",
+  packages=find_packages(),
+  install_requires=[]
+ )
