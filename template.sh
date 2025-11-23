7235e053dedc5b3c24f11a16bac7b6ee5c247ed1
anupriyashahdeo <anupriyashahdeo2001gmail.com>
2025-11-23 16:36:45 +0530

    folder strcuture added

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p research
+
+#creating files
+touch src/_init_.py
+touch app.py
+touch setup.py
+touch src/prompt.py
+touch src/helper.py
+touch research/README.md
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and files created successfully!."
